ae1c50fdcb728ba47a13feaaaa6c053b47c89382
mikefan0503 <fzh2050@gmail.com>
2015-10-14 22:14:48 -0400

    Create running.java

---
+++ running.java
@@ -0,0 +1,39 @@
+import lejos.nxt.*;
+//import lejos.navigation
+import lejos.util.Delay;
+
+public class Running {
+	
+	public static void main(String[] args) {
+		System.out.println("Logical Operator");
+		Button.waitForAnyPress();
+		Boolean run = true;
+		TouchSensor ts = new TouchSensor(SensorPort.S1);
+		//Pilot pl = new Pilot
+		while(run){
+			Motor.B.forward();
+			Motor.C.forward();
+			if(ts.isPressed()){
+				Sound.playTone(600,2000);
+				Motor.B.rotate(-1080,true);
+				Motor.C.rotate(-1080,true);
+				Delay.msDelay(1000);
+				Motor.C.rotate(-385);
+				Delay.msDelay(1000);
+				Motor.B.rotate(360*5,true);
+				Motor.C.rotate(360*5,true);
+				Delay.msDelay(1000);
+				Motor.B.rotate(-385);
+				Delay.msDelay(1000);
+				Sound.playTone(300,2000);
+			}
+			if(Button.ENTER.isPressed()){
+				run = false;
+			}
+		}
+		Button.waitForAnyPress();
+	}
+	
+}
+
+
